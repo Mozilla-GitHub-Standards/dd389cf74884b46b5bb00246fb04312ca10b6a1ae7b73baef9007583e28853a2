0f02e975a7a6e88a35c5a67d54d173a1092e6d6b
Emma Rose <emrose@mozilla.com>
2018-12-10 18:07:20 -0500

    Set up simple logging to stderr configurable via env var

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate clap;
+extern crate env_logger;
+extern crate iron;
+#[macro_use] extern crate log;
+extern crate serde;
+#[macro_use] extern crate serde_derive;
+extern crate serde_json;
+
+
+use env_logger::{Builder, Target};
+
 fn main() {
-    println!("Hello, world!");
+  let mut logger = Builder::from_default_env();
+  logger.target(Target::Stderr);
+  logger.init();
+
+  error!("Testing");
 }
