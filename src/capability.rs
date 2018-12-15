827a531ae1bf0a42327fe8ada14f99d23316ed9b
Emma Rose <emrose@mozilla.com>
2018-12-15 18:32:41 -0500

    Throwing my code away to start from scratch

--- src/capability.rs
+++
@@ -1,8 +0,0 @@
-/// The `Capability` trait is used to name, restrict, and compose functionality
-/// that results in the mutation of or depends on external state, such as a database.
-pub trait Capability<Operation> {
-  type Output;
-
-  fn perform(&self, op: Operation) -> Self::Output;
-}
-
