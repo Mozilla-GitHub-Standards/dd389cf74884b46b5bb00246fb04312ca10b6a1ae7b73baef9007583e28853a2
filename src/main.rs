33512dd8db24f413cb5edd7b74334a1367b9fb13
Emma Rose <emma.arcadia.rose@gmail.com>
2018-12-15 21:01:54 -0500

    Moving everything to one file for easiness

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 mod proxy;
 
+use iron::{
+  middleware::Handler,
+  prelude::*,
+  status::Status,
+};
+
+
+pub struct Proxy {
+}
 
 fn main() {
   let proxy = proxy::Proxy::new();
   iron::Iron::new(proxy).http("127.0.0.1:8080").unwrap();
+}
+
+impl Proxy {
+  pub fn new() -> Self {
+    Proxy {}
+  }
+}
+
+impl Handler for Proxy {
+  fn handle(&self, req: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((Status::Ok, "Hello world")))
+  }
 }
